b775e2058cbaed010883511085a4d060ed775515
Artem Tkachov <artem116tkachov380@gmail.com>
2025-03-07 12:49:35 +0300

    Добавлено подключение к Railway PostgreSQL

--- server.js
+++ server.js
@@ -16,8 +16,9 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASS,
   port: process.env.DB_PORT,
-  ssl: false  // ВАЖНО! Отключаем SSL
+  ssl: { rejectUnauthorized: false }  // Railway требует SSL
 });
+
 
 // Проверка подключения к базе
 pool.connect()
